66263175e748a36851cf8a3cf534443c7fefa476
vshakya <vshakya@redhat.com>
2020-05-26 12:42:56 -0400

    fetchdata updated with search utility and table modified

--- src/app/Expandable/ExpandableDiv.tsx
+++ src/app/Expandable/ExpandableDiv.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import "@patternfly/react-core/dist/styles/base.css";
 import '@app/app.css';
-import { SearchData } from '@app/fetchdata/searchData';
+import { Fetchdata } from '@app/fetchdata/fetchdata';
 import { Expandable } from '@patternfly/react-core';
 
 const ExpandableDiv: React.FunctionComponent = () => (
     <Expandable toggleTextExpanded="Hide Cluster Info" toggleTextCollapsed="Show Cluster Info">
         {/* <Fetchdata /> */}
-        <SearchData>
+        <Fetchdata>
             
-        </SearchData>
+        </Fetchdata>
     </Expandable>
 )
 export { ExpandableDiv };
--- src/app/fetchdata/fetchdata.tsx
+++ src/app/fetchdata/fetchdata.tsx
@@ -1,58 +1,176 @@
-import React, { useState, useEffect } from 'react'
-import ReactDOM from 'react-dom'
-import axios from 'axios'
+import React from "react";
+// import ReactDOM from "react-dom";
+import axios from "axios";
+import "@patternfly/react-core/dist/styles/base.css";
+// import {
+//     Table,
+//     TableHeader,
+//     TableBody,
+//     TableVariant
+// } from '@patternfly/react-table';
+import '../app.css';
+import { PageSection } from '@patternfly/react-core';
+import { any } from 'prop-types';
 
+let cluster_data = [] as any;
+
+const columns = [
+
+    {
+        title: "namespace"
+
+    }, {
+        title: 'node'
+
+    }, {
+        title: 'period_end'
+
+    },
+    {
+        title: 'period_start'
+
+    },
+    {
+        title: 'pod'
+
+    },
+    {
+        title: 'pod_usage_cpu_core_seconds'
+
+    },
+
+]
+
+let rows = [] as any;
 const Fetchdata: React.FunctionComponent<{}> = () => {
-  const url = 'https://bc2f9a92-a71f-4ad8-b93b-63ad65e50fa0.mock.pstmn.io/cluster_data'
+    const url = "https://bc2f9a92-a71f-4ad8-b93b-63ad65e50fa0.mock.pstmn.io/cluster_data";
 
-  const [data, setData] = useState([])
-  const [rows, setRows] = useState([])
+    // store the api data in people array and searchResults
+    React.useEffect(() => {
+        axios.get(url).then(res => {
+            cluster_data = res.data;
 
-  useEffect(() => {
-    axios.get(url).then(json => {
-      setData(json.data);
-      //console.log(json.data);
-      setRows(json.data);
+            // rows = cluster_data.map(item => {
+
+            //     const cell1 = item.namespace;
+            //     //console.log(cell1);
+            //     const cell2 = item.node;
+            //     const cell3 = item.period_end;
+            //     const cell4 = item.period_start;
+            //     const cell5 = item.pod;
+            //     const cell6 = item.pod_usage_cpu_core_seconds;
+            //     const arr = [] as any;
+            //     arr.push(cell1, cell2, cell3, cell4, cell5, cell6);
+            //     //console.log(arr);
+
+            //     return (
+
+            //         arr
+            //     )
+
+            // }
+
+            // )
+            setSearchResults(cluster_data);
+            //console.log(data_row);
+            //rows.push(data_row);
+            //console.log(rows);
+
+            //rows = res.data;
+        });
+    }, []);
+    //console.log(people);
+    const [searchTerm, setSearchTerm] = React.useState("");
+    const [searchResults, setSearchResults] = React.useState([]);
+
+    // habdlechange function to be called when we enter any string in the searchbox, stores the input in varialbe searchTerm
+    const handleChange = e => {
+        setSearchTerm(e.target.value);
+    };
+
+    // apply filter on cluster_data and store results in searchResults
+    React.useEffect(() => {
+        const results = cluster_data.filter(
+            cluster_item =>
+
+                cluster_item.namespace.toLowerCase().includes(searchTerm) ||
+                cluster_item.node.toLowerCase().includes(searchTerm) ||
+                //person.period__end.toDateString().toLowerCase().includes(searchTerm) ||
+                // person.period__start.toLowerCase().includes(searchTerm) ||
+                cluster_item.pod.toLowerCase().includes(searchTerm)
+            //person.pod_usage_cpu_core_seconds.toLowerCase().includes(searchTerm)
+        );
+        //rows = results;
+        setSearchResults(results);
+        console.log(results);
+        //rows = results;
+    }, [searchTerm]);
+
+    // render Table UI
+    return (
+        <PageSection>
+
+            <div className="App">
+                <input
+                    type="text"
+                    placeholder="Search"
+                    value={searchTerm}
+                    onChange={handleChange}
+                />
+
+                <table id="cluster_info">
+
+                    <thead>
+                        <tr>
+                            <th>Namespace</th>
+                            <th>Node</th>
+                            <th>Period_End</th>
+                            <th>Period_Start</th>
+                            <th>Pod</th>
+                            <th>Pod_Usage_Cpu_Core_seconds</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {searchResults.map((cluster_info: { namespace: React.ReactNode; node: React.ReactNode; period_end: React.ReactNode; period_start: React.ReactNode; pod: React.ReactNode; pod_usage_cpu_core_seconds: React.ReactNode; }) => {
+                            return (
+                                <React.Fragment>
+                                    <tr>
+                                        <td>{cluster_info.namespace}</td>
+                                        <td>{cluster_info.node}</td>
+                                        <td>{cluster_info.period_end}</td>
+                                        <td>{cluster_info.period_start}</td>
+                                        <td>{cluster_info.pod}</td>
+                                        <td>{cluster_info.pod_usage_cpu_core_seconds}</td>
+                                    </tr>
+                                </React.Fragment>
+
+                            )
 
 
-    })
-  }, [])
+                        })}
 
-  console.log(rows);
 
-  const renderTable = () => {
 
-    return data.map(cluster_info => {
-      return (
-        <tr>
-          <td>{cluster_info.namespace}</td>
-          <td>{cluster_info.node}</td>
-          <td>{cluster_info.period__end}</td>
-          <td>{cluster_info.period_start}</td>
-          <td>{cluster_info.pod}</td>
-          <td>{cluster_info.pod_usage_cpu_core_seconds}</td>
-        </tr>
-      )
-    })
-  }
+                    </tbody>
+                </table>
 
-  return (
-    <div>
-      <h1 id="title">Cluster Info Table</h1>
-      <table id="cluster_info">
-        <thead>
-          <tr>
-            <th>Namespace</th>
-            <th>Node</th>
-            <th>Period_End</th>
-            <th>Period_Start</th>
-            <th>Pod</th>
-            <th>Pod_Usage_Cpu_Core_seconds</th>
-          </tr>
-        </thead>
-        <tbody>{renderTable()}</tbody>
-      </table>
-    </div>
-  )
+                {/* <Table aria-label="Compact Table" variant={TableVariant.compact} cells={columns} rows={rows}>
+
+
+                    <TableHeader />
+                    <TableBody>
+
+
+                    </TableBody>
+
+
+                </Table> */}
+            </div>
+
+        </PageSection>
+
+    );
 }
+
 export { Fetchdata };
