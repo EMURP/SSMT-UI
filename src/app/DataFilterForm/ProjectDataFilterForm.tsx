f7badadacd19d61b1f4ba8a3e2512777624dad35
Nikunj Doshi <ndoshi@redhat.com>
2020-06-10 00:57:46 -0400

    Fixed Date Issue

--- src/app/DataFilterForm/ProjectDataFilterForm.tsx
+++ src/app/DataFilterForm/ProjectDataFilterForm.tsx
@@ -16,114 +16,127 @@ import { Button } from '@patternfly/react-core';
 
 import { Fetchdata } from '@app/fetchdata/Fetchdatatemp';
 
-
 type myProps = {};
 type myState = {
-    startHrs: number;
-    endHrs: number;
-    startDate: Date;
-    endDate: Date;
-    submitToggle: boolean;
-    conditionalRender: number;
-    changingDate: boolean;
+  startHrs: number;
+  endHrs: number;
+  startDate: Date;
+  endDate: Date;
+  submitToggle: boolean;
+  conditionalRender: number;
+  changingDate: boolean;
 };
 
-
 const horizontalFlex = {
-    display: 'flex',
-    flexDirection: 'row'
-  } as React.CSSProperties;
-  
-  const padding = {
-    marginRight: '30px'
-  } as React.CSSProperties;
-  
+  display: 'flex',
+  flexDirection: 'row'
+} as React.CSSProperties;
 
-const convertDateToUTC = (date: Date, hrs: number )=> { 
-    return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
-     hrs, 0, 0); }
+const padding = {
+  marginRight: '30px'
+} as React.CSSProperties;
 
-// const convertDateToUTC = (date: Date, hrs: number )=> { 
-//       return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
-//        hrs, 0, 0); }
- 
-class ProjectDataFilterForm extends  React.Component<myProps, myState> {
+const convertDateToUTC = (date: Date) => {
+  return new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), 0, 0,0));
+};
 
-    constructor(myProps) {
+class ProjectDataFilterForm extends React.Component<myProps, myState> {
+  constructor(myProps) {
+    super(myProps);
 
-      super(myProps);
+    const startDate = new Date();
+    startDate.setHours((new Date().getHours()) - 1 )
+    startDate.setMinutes(0);
+    startDate.setSeconds(0);
+    const endDate = new Date();
+    endDate.setHours(new Date().getHours())
+    endDate.setMinutes(0);
+    endDate.setSeconds(0);
+    console.log(startDate);
+    console.log(endDate);
+    this.state = {
+      startHrs: ((new Date().getHours() - 1 +24) % 24), // javascript Modulo function is buggy
+      endHrs: (new Date().getHours() % 24),
+      startDate: new Date(startDate),
+      endDate: new Date(endDate),
+      submitToggle: false,
+      conditionalRender: 0,
+      changingDate: true
+    };
 
-      this.state={
-          startHrs: new Date().getHours()-1,
-          endHrs: new Date().getHours(),
-
-          // this.state={
-          //   startHrs: new Date().getHours()-1,
-          //   endHrs: new Date().getHours(),
-          startDate: convertDateToUTC(new Date(),new Date().getHours()),
-          endDate: convertDateToUTC(new Date(),new Date().getHours()+1),
-          submitToggle: false,
-          conditionalRender:0,
-          changingDate: true,
-      }
-
-      console.log(this.state)
-    }
-
-    // shouldComponentUpdate(nextProps: myProps, nextState: myState){
-    //   return this.state.conditionalRender!==nextState.conditionalRender;
-    // }
-
-    changeToggle=() =>{
-      const conditionalRender: number= this.state.conditionalRender;
-      if(this.state.startDate !== convertDateToUTC(new Date(Date.UTC(0, 0, 0, 0, 0, 0)),0)){
-        this.setState({...this.state, changingDate:false ,submitToggle:true, conditionalRender:conditionalRender+1 })
-      }
-    }
-
-    setStartHrs = (hrs: number) => {
-        this.setState({...this.state, changingDate:true, startHrs:hrs,startDate:convertDateToUTC(new Date(this.state.startDate),hrs)});
-    }
-
-    setEndHrs = (hrs: number) => {
-      this.setState({...this.state,  changingDate:true,  endHrs:hrs,endDate:convertDateToUTC(new Date(this.state.endDate),hrs)})
-    }
-
-    setStartDate = (date: Date) => {
-      this.setState({...this.state,  changingDate:true, startDate:convertDateToUTC(new Date(date),this.state.startHrs)})
   }
+
+
+  changeToggle = () => {
+    const conditionalRender: number = this.state.conditionalRender;
+      this.setState({
+        ...this.state,
+        changingDate: false,
+        submitToggle: true,
+        conditionalRender: conditionalRender + 1
+      });
+  };
+
+  setStartHrs = (hrs: number) => {
+    const date=new Date(this.state.startDate)
+    date.setHours(hrs)
+    this.setState({
+      ...this.state,
+      changingDate: true,
+      startHrs: hrs % 24,
+      startDate: new Date(date),
+    });
+  };
+
+  setEndHrs = (hrs: number) => {
+    const date=new Date(this.state.endDate)
+    date.setHours(hrs)
+    this.setState({
+      ...this.state,
+      changingDate: true,
+      endHrs: hrs % 24,
+      endDate: new Date(date)
+    });
+  };
+
+  setStartDate = (date: Date) => {
+    date=new Date(date)
+    date.setHours(this.state.startHrs)
+    date.setDate(date.getDate()+1)
+    console.log(date)
+    this.setState({ ...this.state, changingDate: true, startDate: new Date(date) });
+  };
 
   setEndDate = (date: Date) => {
-    this.setState({...this.state, changingDate:true ,endDate:convertDateToUTC(new Date(date),this.state.startHrs)})
-  }
+    date=new Date(date)
+    date.setHours(this.state.endHrs)
+    date.setDate(date.getDate()+1)
+    console.log(date)
+    this.setState({ ...this.state, changingDate: true, endDate: new Date(date) });
+  };
 
-
-    render() {
-
-        return(
-            <div>
-            <div>
+  render() {
+    return (
+      <div>
+        <div>
           <h1>Active Time </h1>
         </div>
 
         <div style={horizontalFlex}>
           <div style={padding}>
-
             <Card>
               <CardBody> Start Date: </CardBody>
             </Card>
 
-            <SimpleInputGroups changeDate={this.setStartDate} dateType="startDate" key="startDate"/>
-            {this.state.startDate.toISOString()}
-        
+            <SimpleInputGroups changeDate={this.setStartDate} dateType="startDate" key="startDate" />
+            {convertDateToUTC(this.state.startDate).toISOString()}
+
             <Card>
               <CardBody> Start Time: </CardBody>
             </Card>
 
-          <DropdownComponent key={"startHrs"} setHrs={this.setStartHrs} Hrs={this.state.startHrs} />         
-          {this.state.startHrs} 
-
-
+            <DropdownComponent key={'startHrs'} setHrs={this.setStartHrs} Hrs={this.state.startHrs} />
+            {this.state.startHrs}
           </div>
 
           <div style={padding}>
@@ -131,38 +144,42 @@ class ProjectDataFilterForm extends  React.Component<myProps, myState> {
               <CardBody> End Date: </CardBody>
             </Card>
 
-            <SimpleInputGroups changeDate={this.setEndDate} dateType="endDate" key="endDate"/>
-            {this.state.endDate.toISOString()}
-
+            <SimpleInputGroups changeDate={this.setEndDate} dateType="endDate" key="endDate" />
+            {convertDateToUTC(this.state.endDate).toISOString()}
             <Card>
               <CardBody> End Time: </CardBody>
             </Card>
 
-         <DropdownComponent key={"endHrs"} setHrs={this.setEndHrs} Hrs={this.state.endHrs}/>
-         {this.state.endHrs}
-         
+            <DropdownComponent key={'endHrs'} setHrs={this.setEndHrs} Hrs={this.state.endHrs} />
+            {this.state.endHrs}
           </div>
-
 
           <div>
-            <Button isBlock onClick={()=>this.changeToggle()} disabled={this.state.startDate !== convertDateToUTC(new Date(Date.UTC(0, 0, 0, 0, 0, 0)),0)}>Search</Button>
+            <Button
+              isBlock
+              onClick={() => this.changeToggle()}
+            >
+              Search
+            </Button>
           </div>
         </div>
 
-        {<div>
-          <h2>Data from Fetch Data </h2>
+        {
+          <div>
+            <h2>Data from Fetch Data </h2>
 
-        <Fetchdata key={"Data"} changingDate={this.state.changingDate} renderCount={this.state.conditionalRender} searching={this.state.submitToggle}startDate={this.state.startDate} endDate={this.state.endDate}/>
-
-        
-        </div> }
-
-        </div>
-        );
-
-    }
-
-
+            <Fetchdata
+              key={'Data'}
+              changingDate={this.state.changingDate}
+              renderCount={this.state.conditionalRender}
+              searching={this.state.submitToggle}
+              startDate={convertDateToUTC(this.state.startDate)}
+              endDate={convertDateToUTC(this.state.endDate)}
+            />
+          </div>
+        }
+      </div>
+    );
+  }
 }
-export {ProjectDataFilterForm};
-
+export { ProjectDataFilterForm };
