1e8a304488c5693fdabbbf75a18d9323380c4fd7
vshakya <vshakya@redhat.com>
2020-06-11 12:50:34 -0400

    PR changes

--- src/app/project_page/project_detail.tsx
+++ src/app/project_page/project_detail.tsx
@@ -5,77 +5,78 @@ import axios from 'axios';
 
 let cluster_data = [] as any;
 const ProjectDetail: React.FunctionComponent<{}> = () => {
-    const [name,setName] = React.useState("")
-    const [node,setNode] = React.useState("")
-    const [start,setStart] = React.useState("")
-    const [end,setEnd] = React.useState("")
-    const [pod,setPod] = React.useState("")
-    const [cpusage,setCpusage] = React.useState("")
+    const [name, setName] = React.useState("")
+    const [node, setNode] = React.useState("")
+    const [start, setStart] = React.useState("")
+    const [end, setEnd] = React.useState("")
+    const [pod, setPod] = React.useState("")
+    const [cpusage, setCpusage] = React.useState("")
 
     let { projectId } = useParams();
     console.log(projectId)
     React.useEffect(() => {
-        axios.get("https://c507295a-b340-4a31-a144-749e6fb4c08a.mock.pstmn.io/project_list_with_activation_time/"+projectId).then(res => {
+        axios.get("https://c507295a-b340-4a31-a144-749e6fb4c08a.mock.pstmn.io/project_list_with_activation_time/" + projectId).then(res => {
             cluster_data = res.data;
 
             cluster_data.map((item: { namespace: any; }) => {
-                console.log(typeof(item.namespace))
+                console.log(typeof (item.namespace))
                 setName(item.namespace)
                 setNode(item.node)
                 setStart(item.period_start)
                 setEnd(item.period_end)
                 setPod(item.pod)
                 setCpusage(item.pod_usage_cpu_core_seconds)
-                
-            })         
 
-        }).catch(err =>{ if(err.response){
-            console.log(err.response +"--"+err.message)
-        }
-        else{
-            console.log(err.message)
+            })
 
-        }
-    });
+        }).catch(err => {
+            if (err.response) {
+                console.log(err.response + "--" + err.message)
+            }
+            else {
+                console.log(err.message)
+
+            }
+        });
     }, []);
 
-   
+
     return (
 
         <PageSection>
             <Title headingLevel="h1" size="lg">Projectpage</Title>
-            
+
             <div>
 
-               {name}
+                Project Name: {name}
 
             </div>
             <div>
 
-                {node}
+                Node: {node}
 
             </div>
             <div>
 
-                {start}
+                Period Start Time: {start}
 
             </div>
             <div>
 
-                {end}
+                Period End Time: {end}
 
             </div>
             <div>
 
-                {pod}
+                Pod: {pod}
 
             </div>
             <div>
 
-                {cpusage}
+                Pod_usage_cpu_core_seconds: {cpusage}
 
-            </div>    
-           
+            </div>
+
         </PageSection>
 
 
