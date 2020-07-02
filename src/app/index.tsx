b2fe993e4fb6f3036784538e74e4bc9ff1464f30
surbhi kanthed <surbhikanthed@localhost.localdomain>
2020-07-01 22:51:56 -0400

    resolved DashboardTable.tsx and app/index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,13 +6,52 @@ import { AppRoutes } from '@app/routes';
 import '@app/app.css';
 
 
-const App: React.FunctionComponent = () => (
-  <Router>
-    <AppLayout>
-      <AppRoutes />
-    </AppLayout>
-  </Router>
-);
+type myState = {
+  role: Role;
+};
+
+export const enum Role {
+  ADMIN,
+  DEVELOPER,
+  NONE
+}
+
+export const RoleMap = ["Admin","Developer"]
+type myProps = {};
+
+class App extends React.Component<myProps, myState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      role: Role.NONE
+    };
+  }
+
+  handleRoleChange = (role: Role) => {
+    this.setState({ role: role });
+  }
+
+  logout = ()=> {
+    this.setState({ role: Role.NONE });
+  }
+
+
+  render() {
+    return (
+      <div>
+        {this.state.role=== Role.NONE && <LoginPage setRole={this.handleRoleChange}/>}
+        {this.state.role !== Role.NONE && (
+          <Router>
+            <AppLayout role={this.state.role} logout={this.logout}>
+              <AppRoutes />
+            </AppLayout>
+          </Router>
+        )}
+      </div>
+    );
+  }
+
+}
 
 
 export {App};
