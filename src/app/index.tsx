6a0fc44e51a8b2b64cfe926e8785c9b3dbe6905f
surbhi kanthed <surbhikanthed@localhost.localdomain>
2020-07-01 23:41:19 -0400

    app/index.tsx resolved

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,6 +7,7 @@ import '@app/app.css';
 import { LoginPage } from './LoginPage/LoginPage';
 
 
+
 type myState = {
   role: Role;
 };
@@ -53,7 +54,4 @@ class App extends React.Component<myProps, myState> {
   }
 
 }
-
-
 export {App};
-
