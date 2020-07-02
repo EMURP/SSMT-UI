2c7d8e4f84a0dd949c28057fc7a96a0877a16b05
surbhi kanthed <surbhikanthed@localhost.localdomain>
2020-07-02 00:32:27 -0400

    app/index.tsx  resolved

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -25,7 +25,6 @@ class App extends React.Component<myProps, myState> {
   handleRoleChange = (role: Role) => {
     this.setState({ role: role });
   }
-
   logout = ()=> {
     this.setState({ role: Role.NONE });
   }
